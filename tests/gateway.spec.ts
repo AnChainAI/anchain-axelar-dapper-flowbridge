02b16ed9c6cf4235a414aa8e6a6251072ae6ea2e
thhachain <thanh.ha@anchain.ai>
2023-10-10 12:32:34 -0700

    Adding testing for approving contract call. Need to figure out how to generate a hex encoded public key from EVM operators to be used for verifying messages on Flow

--- tests/gateway.spec.ts
+++ tests/gateway.spec.ts
@@ -1,16 +1,32 @@
 import { AxelarAuthWeightedContract } from './contracts/axelar-auth-weighted.contract'
 import { IAxelarExecutableContract } from './contracts/i-axelar-executable.contract'
 import { EternalStorageContract } from './contracts/eternal-storage.contract'
+import { dataToHexEncodedMessage } from './utils/data-to-hex-encoded-message'
 import { AxelarGatewayContract } from './contracts/axelar-gateway.contract'
 import { Emulator, FlowAccount, EMULATOR_CONST } from '../utils/testing'
+import { deployAuthContract } from './transactions/deploy-auth-contract'
 import { getDeployedContracts } from './scripts/get-deployed-contracts'
 import { deployContracts } from './transactions/deploy-contracts'
 import { callContract } from './transactions/call-contract'
+import { execute } from './transactions/execute'
 import { FlowConstants } from '../utils/flow'
+import { randomUUID } from 'crypto'
+import { ethers } from 'hardhat'
+import { sortBy } from 'lodash'
 
 // npm test -- gateway.spec.ts
 describe('AxelarGateway', () => {
+  const defaultAbiCoder = ethers.AbiCoder.defaultAbiCoder()
+  const wallets = Array.from({ length: 10 }).map(() =>
+    ethers.Wallet.createRandom(),
+  )
+  const keccak256 = ethers.keccak256
   const encoder = new TextEncoder()
+  const user = wallets[0]
+  const threshold = 7
+  const operators = sortBy(wallets.slice(0, threshold), (wallet) =>
+    wallet.address.toLowerCase(),
+  )
   let constants: FlowConstants
   let relayer: FlowAccount
   let admin: FlowAccount
@@ -32,7 +48,17 @@ describe('AxelarGateway', () => {
       const axelarAuthWeightedContract = AxelarAuthWeightedContract()
       await deployContracts({
         args: {
-          contracts: [iAxelarExecutableContract, axelarAuthWeightedContract],
+          contracts: [iAxelarExecutableContract],
+        },
+        authz: admin.authz,
+      })
+      await deployAuthContract({
+        args: {
+          contractName: axelarAuthWeightedContract.name,
+          contractCode: axelarAuthWeightedContract.code,
+          recentOperators: operators.map((o) => o.address.toLowerCase()),
+          recentWeights: operators.map(() => 1),
+          recentThreshold: operators.length,
         },
         authz: admin.authz,
       })
@@ -70,23 +96,20 @@ describe('AxelarGateway', () => {
     it('should emit call contract event', async () => {
       // Create a relayer account for sending transactions
       relayer = await FlowAccount.from({})
-
       // Create input params for callContract
       const data = {
         destinationChain: 'Destination',
         destinationContractAddress: '0x123abc',
         payload: Array.from(
-          encoder.encode(JSON.stringify({ address: relayer.addr })),
+          encoder.encode(defaultAbiCoder.encode(['address'], [user.address])),
         ),
       }
-
       // Send callContract transaction
       const tx = await callContract({
         constants,
         args: data,
         authz: relayer.authz,
       })
-
       // Expect that an event is emitted and input params matching
       expect(tx.events).toEqual(
         expect.arrayContaining([
@@ -103,6 +126,73 @@ describe('AxelarGateway', () => {
           }),
         ]),
       )
+    })
+  })
+
+  describe('Approve Contract Call', () => {
+    it('should approve a contract call', async () => {
+      const payload = defaultAbiCoder.encode(['address'], [user.address])
+      const sourceChain = 'Source'
+      const sourceAddress = 'address0x123'
+      const contractAddress = user.address
+      const payloadHash = keccak256(payload)
+      const sourceTxHash = keccak256('0x123abc123abc')
+      const sourceEventIndex = 17
+      const commandId = randomUUID()
+
+      const approveData = dataToHexEncodedMessage(
+        [commandId],
+        ['approveContractCall'],
+        [
+          [
+            sourceChain,
+            sourceAddress,
+            contractAddress,
+            payloadHash,
+            sourceTxHash,
+            sourceEventIndex.toString(),
+          ],
+        ],
+      )
+
+      const signatures = await Promise.all(
+        sortBy(operators, (wallet) => wallet.address.toLowerCase()).map(
+          (wallet) =>
+            wallet.signMessage(keccak256(encoder.encode(approveData))),
+        ),
+      )
+
+      // TODO: Generate a public key from EVM operators
+      const pubKey = operators[0].signingKey.publicKey.slice(4)
+      const hexKey = Buffer.from(pubKey).toString('hex')
+      console.log(hexKey)
+
+      // const tx = await execute({
+      //   constants,
+      //   args: {
+      //     commandIds: [commandId],
+      //     commands: ['approveContractCall'],
+      //     params: [
+      //       [
+      //         sourceChain,
+      //         sourceAddress,
+      //         contractAddress,
+      //         payloadHash,
+      //         sourceTxHash,
+      //         sourceEventIndex.toString(),
+      //       ],
+      //     ],
+      //     operators: operators.map((operator) =>
+      //       operator.address.toLowerCase(),
+      //     ),
+      //     weights: operators.map(() => 1),
+      //     threshold: operators.length,
+      //     signatures,
+      //   },
+      //   authz: relayer.authz,
+      // })
+
+      // console.log(JSON.stringify(tx, null, 2))
     })
   })
 })
